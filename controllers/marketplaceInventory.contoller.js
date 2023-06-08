293b3d717a683f01791542b84a31593b08fc7626
yakshith <yakshith027@gmail.com>
2023-06-09 03:04:12 +0530

    color bug fixed

--- controllers/marketplaceInventory.contoller.js
+++ controllers/marketplaceInventory.contoller.js
@@ -21,7 +21,7 @@ const getCars = async (req, res) => {
     if (maxPrice && minPrice) obj.price = { $gte: +minPrice, $lte: +maxPrice }
     if (maxMileage && minMileage)
       obj['OEM.mileage'] = { $gte: minMileage, $lte: maxMileage }
-    if (color) obj.orginalPaint = ['red']
+    if (color) obj.orginalPaint = color
     if (text) obj.title = { $regex: text, $options: 'i' }
 
     page = page || 1
